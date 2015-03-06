b198d3ad3825d8f3b808225c6e6e87f26cf1be4a
Chaichalit Srisawat <boywert@gmail.com>
2015-03-06 17:09:32 +0000

    no message

--- io/io_cubep3m.c
+++ io/io_cubep3m.c
@@ -10,8 +10,10 @@
 #include "../particle.h"
 #include "../config_vars.h"
 
-void string_replace(char *out, char *in, char *find, char *replace) {
-  char *p;
+int string_replace_getblock(char *out, char *in, char *find, char *replace) {
+  char *p,*q;
+  char buf[1000] = {"\0"};
+  int block;
   int i,j=0;
   int lenin = strlen(in);
   int lenfind = strlen(find);
@@ -28,25 +30,48 @@ void string_replace(char *out, char *in, char *find, char *replace) {
     for(i=0;i<lenin-first_replace-lenfind;i++) {
       out[first_replace+lenreplace+i] = in[first_replace+lenfind+i];j++;
     }
-    //strcat(out,"\0");
+    // get block by checking the number before .dat
+    q = strstr(in,".dat");
+    int last_block_pos = q-&in[0];
+    int first_block_pos = first_replace+lenfind;
+    int blocklen = last_block_pos-first_block_pos;
+    strncpy(buf,&in[first_block_pos],blocklen);
+    sscanf(buf,"%d",&block);
+    return block;
   }
-  else
+  else {
     sprintf(out,"%s",in);
+    return -1;
+  }
   // printf("expect = %d, real = %d, count = %d\n",lenin+lenreplace-lenfind,strlen(out),j);
 }
 
-void rescale_xv(float *xv, int np_local, float a) {
+void rescale_xv(float *xv, int np_local, int block, float a) {
   float H0 = 100.;   //[h*km]/[sec*Mpc]
   float RHO_CRIT_0 = 2.7755397e11;   //[h^2*Msun]/[Mpc^3]
   float Omega0 = 0.3;
   float vunit_compute = BOX_SIZE * 1.5 * sqrt(Omega0) * H0 / (2.*(float)CUBEP3M_NP)/a;  //!km/s
   float munit_compute = BOX_SIZE * BOX_SIZE * BOX_SIZE * Omega0 * RHO_CRIT_0 / ((float)CUBEP3M_NP*(float)CUBEP3M_NP*(float)CUBEP3M_NP); // ! Msun/h
   float  lunit_compute = BOX_SIZE/(2.*(float)CUBEP3M_NP); //Mpc/h
-  int i;
+  int i,j,k;
+  float offset1,offset2,offset3;
+  for(k=0;k<CUBEP3M_NDIM;k++)
+    for(j=0;j<CUBEP3M_NDIM;j++)
+      for(i=0;i<CUBEP3M_NDIM;i++) {
+	if(block == i+j*CUBEP3M_NDIM+k*CUBEP3M_NDIM*CUBEP3M_NDIM) {
+	  offset1 = (float)i*BOX_SIZE/(float)CUBEP3M_NDIM;
+	  offset2 = (float)j*BOX_SIZE/(float)CUBEP3M_NDIM;
+	  offset3 = (float)k*BOX_SIZE/(float)CUBEP3M_NDIM;
+	}
+      }
+  
   for(i=0;i<np_local;i++){
     xv[i] *= lunit_compute;
+    xv[i] += offset1;
     xv[i+1] *= lunit_compute;
+    xv[i+1] += offset2;
     xv[i+2] *= lunit_compute;
+    xv[1+2] += offset3;
     xv[i+3] *= vunit_compute;
     xv[i+4] *= vunit_compute;
     xv[i+5] *= vunit_compute;
@@ -59,7 +84,8 @@ void load_particles_cubep3m(char *filename, struct particle **p, int64_t *num_p)
   char xvfile[1024], PIDfile[1024];
   char buffer[1024] = {'\0'};
   float *xv;
-  int64_t i,n, *PID;
+  int64_t i, *PID;
+  int block;
   struct cubep3m_header {
     int np_local;
     float a,t,tau;
@@ -69,9 +95,9 @@ void load_particles_cubep3m(char *filename, struct particle **p, int64_t *num_p)
     float mass_p;
   } header1,header2;
   printf("filename = %s\n",filename);
-  string_replace(buffer,filename,"xvPID","xv");
+  block = string_replace_getblock(buffer,filename,"xvPID","xv");
   strcpy(xvfile,buffer);
-  string_replace(buffer,filename,"xvPID","PID");
+  block = string_replace_getblock(buffer,filename,"xvPID","PID");
   strcpy(PIDfile,buffer);
   printf("xv = %s, pid = %s\n",xvfile,PIDfile);
   input = check_fopen(xvfile,"rb");
@@ -85,7 +111,7 @@ void load_particles_cubep3m(char *filename, struct particle **p, int64_t *num_p)
   fread(xv, sizeof(float),6*header1.np_local, input);
   fclose(input);
 
-  rescale_xv(xv, header1.np_local, header1.a);  
+  rescale_xv(xv, header1.np_local, block, header1.a);  
 
   for(i=0;i<header1.np_local;i++) {
     memcpy(&((*p)[(*num_p)+i].pos[0]),&(xv[i*6]),sizeof(float)*6);
